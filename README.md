375bdab41eeb8db834102a3d2401b340ae87815c
RhindorOP <helloassass@gmail.com>
2021-08-16 23:43:02 +0400

    Updated Appbar

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Lee Kang-Jin 1
 
-Hey there! This is my attempt at translating Lee Kang Jin 1, with BushRanger doing the editing. Please feel free to contact me if you would like to help with the translations or anything else through reddit, u/RhindorOP, or the discord server. 
+Hey there! This is my attempt at translating Lee Kang Jin 1, with BushRanger doing the editing. Please feel free to contact me if you would like to help with the translations or anything else through reddit, u/RhindorOP.
 
 ## Website Link
 [rhindorop.github.io/LKJ1](https://rhindorop.github.io/LKJ1/)
--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,55 @@
 <template>
-  <v-app>
-    <v-app-bar
-      absolute
-      color="indigo darken-4"
-      class="d-flex justify-center text-h4"
-      dark
-    >
-        Lee Kang-Jin 1
-    </v-app-bar>
-    <v-main>
-      <Story/>
-    </v-main>
-  </v-app>
+	<v-app>
+		<v-app-bar color="indigo darken-4" dark app hide-on-scroll>
+			<v-card
+				style="width:100vw"
+				class="d-flex justify-space-between align-center text-h4"
+				color="transparent"
+				flat
+			>
+				<v-menu offset-y>
+					<template v-slot:activator="{ on, attrs }">
+						<v-btn icon v-bind="attrs" v-on="on">
+							<v-icon>mdi-menu</v-icon>
+						</v-btn>
+					</template>
+					<v-list>
+						<v-list-item class="pa-0">
+							<v-btn href="https://rhindorop.github.io/LKJ1/" text>LKJ 1</v-btn>
+						</v-list-item>
+						<v-list-item class="pa-0">
+							<v-btn href="https://rhindorop.github.io/LKJ2/" text>LKJ 2</v-btn>
+						</v-list-item>
+					</v-list>
+				</v-menu>
+
+				<div class="d-flex">
+					Admin
+					<div class="d-none d-sm-flex">istrator &zwnj;</div>
+					<div class="d-none d-sm-flex">Lee Kang-Jin &zwnj;</div>
+					<div class="d-flex d-sm-none">&zwnj; LKJ &zwnj;</div>1
+				</div>
+				<div></div>
+			</v-card>
+		</v-app-bar>
+		<v-main>
+			<Story />
+		</v-main>
+	</v-app>
 </template>
 
 <script>
-import Story from './components/Story';
+import Story from "./components/Story";
 
 export default {
-  name: 'App',
+	name: "App",
 
-  components: {
-    Story,
-  },
+	components: {
+		Story,
+	},
 
-  data: () => ({
-    //
-  }),
+	data: () => ({
+		//
+	}),
 };
 </script>
